560a0f40992c5cedecb5730bee39c59e0b08950f
Diana Llona <dev.dianallona@gmail.com>
2024-09-11 13:23:54 +0800

    fix navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -11,44 +11,40 @@ const Navbar = () => {
       <div className="flex gap-14">
         <a
           href="/"
-          className="text-xl text-gray-700 hover:font-semibold transition-all ease-in-out delay-150"
+          className="text-xl text-gray-700 hover:font-semibold transition-all ease-in-out duration-150"
         >
           My Listing
         </a>
         <a
           href="/"
-          className="text-xl text-gray-700 hover:font-semibold transition-all ease-in-out delay-150"
+          className="text-xl text-gray-700 hover:font-semibold transition-all ease-in-out duration-150"
         >
           Company
         </a>
         <a
           href="/"
-          className="text-xl text-gray-700 hover:font-semibold transition-all ease-in-out delay-150"
+          className="text-xl text-gray-700 hover:font-semibold transition-all ease-in-out duration-150"
         >
           Profile
         </a>
       </div>
       <img src={logo} alt="logo" className="w-auto h-8 object-contain" />
       <div className="flex items-center">
-        <div className="flex gap-1">
-          <AnnotationAlertIcon className="m-2.5 hover:scale-110 cursor-pointer transition-all ease-in-out delay-150" />
-          <MessagesSquareIcon className="m-2.5 hover:scale-110 cursor-pointer transition-all ease-in-out delay-150" />
-          <BellIcon className="m-2.5 hover:scale-110 cursor-pointer transition-all ease-in-out delay-150" />
+        <div className="flex gap-1 pr-2">
+          <AnnotationAlertIcon className="m-2.5 hover:scale-110 cursor-pointer" />
+          <MessagesSquareIcon className="m-2.5 hover:scale-110 cursor-pointer" />
+          <BellIcon className="m-2.5 hover:scale-110 cursor-pointer" />
         </div>
-        <div className="border-r h-9 mx-2 border-gray-50" />
-        <div className="flex items-center group cursor-pointer">
+        <div className="border-r h-9 border-gray-50" />
+        <div className="flex items-center group cursor-pointer pl-2 hover:bg-gray-100 rounded-sm">
           <img
             src={profileImg}
             alt="profile-img"
             className="rounded-full w-9 h-9"
           />
           <div className="mx-1.5">
-            <p className="text-gray-800 text-body group-hover:font-semibold transition-all ease-in-out delay-150">
-              Juanita Florencia
-            </p>
-            <p className="text-gray-400 text-caption group-hover:font-semibold transition-all ease-in-out delay-150">
-              Member #10293
-            </p>
+            <p className="text-gray-800 text-body">Juanita Florencia</p>
+            <p className="text-gray-400 text-caption">Member #10293</p>
           </div>
           <ChevronDownIcon className="m-2 w-4 h-4 group-hover:scale-110 cursor-pointer" />
         </div>
